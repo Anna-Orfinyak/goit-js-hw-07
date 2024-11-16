586c2fbe56818de46cd369884fc8038452ab4983
Anna Orfinyak <anna.orfinyak@gmail.com>
2024-11-17 00:03:44 +0200

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+    event.preventDefault();
+
+    const elements = loginForm.elements;
+
+    if (!loginForm.elements.email.value || !loginForm.elements.password.value) {
+        alert `All form fields must be filled in`;
+        return;
+    }
+
+    const info = {
+        email: elements.email.value,
+        password: elements.password.value
+    }
+
+    console.log(info);
+
+    loginForm.reset(); 
+}
