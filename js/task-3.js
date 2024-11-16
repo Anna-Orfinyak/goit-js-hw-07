ef0f6a1824d5ad1746b5abbe2728db6816b7698c
Anna Orfinyak <anna.orfinyak@gmail.com>
2024-11-16 21:53:48 +0200

    add task-3

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener('input', () => {
+
+  const enteredName = input.value.trim();
+
+  output.textContent = enteredName || 'Anonymous';
+});
+
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+// На що буде звертати увагу ментор при перевірці:
+// На елементі input#name-input прослуховується подія input
+// Під час набору тексту в інпуті його поточне значення підставляється в span#name-output як ім’я для привітання
+// Значення в інпуті очищене від пробілів по краях
+// Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous"
