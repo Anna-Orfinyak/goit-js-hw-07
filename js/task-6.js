3f008b9481313f79ca3ed7cc65450130d5bf22d1
Anna Orfinyak <anna.orfinyak@gmail.com>
2024-11-17 02:44:40 +0200

    add task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,48 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+let size = 30;
+
+createBtn.addEventListener('click', createMarkup);
+
+function createMarkup() {
+  const value = Number(input.value);
+  if (value <= 0 || value > 100) {
+    alert('Please enter a number between 1 and 100');
+    return;
+  }
+  
+  createBoxes(value);
+}
+
+function createBoxes(number) {
+  const boxesContainer = [];
+
+  for(let i = 0; i < number; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.push(box);
+
+    size += 10;
+  }
+
+  boxes.innerHTML = '';
+  boxes.append(...boxesContainer);
+  input.value ='';
+  size = 30;
+};
+
+function destroyBoxes () {
+  boxes.innerHTML = '';
+  size = 30;
+}
+destroyBtn.addEventListener('click', destroyBoxes);
